14b55bc574bd508508a12cf43bc53fd24ffe6fef
Betul Kaplan <betulkaplan7@gmail.com>
2023-06-08 23:27:58 +0200

    list properties in dashboard

--- src/app/features/components/dashboard/dashboard.component.ts
+++ src/app/features/components/dashboard/dashboard.component.ts
@@ -1,10 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { PropertyService } from '../../service/property.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent {
+  properties: any[] = [];
+  constructor(private propertyService: PropertyService) {}
 
+  ngOnInit(): void {
+    this.getProperties();
+  }
+
+  getProperties(): void {
+    this.propertyService.getProperties().subscribe(
+      (properties) => (this.properties = properties),
+
+      (error) => console.error(error)
+    );
+  }
 }
